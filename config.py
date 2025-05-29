3005da7a6edffc431cd8bf13efff5fa91d066f2e
Paushigaa <paushigaa.s@goml.io>
2025-05-29 14:15:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,1 @@
-class Config:
-    LYZR_API_KEY: str = "sk-default-OxZ6tvVu12kvRx7fPoYJea7zuw4SHIDE"
-    LYZR_API_URL: str = "https://agent-prod.studio.lyzr.ai/v3/inference/chat/"
 
-    USER_ID: str = "paushigaa.s@goml.io"
-
-    AGENT_ID: str = "6838040868eac2f109498531"
-    SESSION_ID: str = "6838040868eac2f109498531-9yhize251im"
